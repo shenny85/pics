fe057094b755bb23ab947e7a4e00fe44763a1d8e
shenny85 <143647445+shenny85@users.noreply.github.com>
2023-11-03 17:35:00 -0400

    added Http request for data fetching

---
+++ src/api.js
@@ -0,0 +1,19 @@
+//code related to contacting the Unsplash API
+import axios from 'axios';
+
+const searchImages = async(term) => {
+   const response = await axios.get('https://api.unsplash.com//search/photos', {
+        headers: {
+            Authorization: 'Client-ID AOFombQLbroDmmLQr9tHvvPCduln-IJ8yLWfXqdaaiI'
+        },
+        params: {
+           query: term, 
+
+        }
+    });
+
+
+    return response.data.results;
+};
+
+export default searchImages;
